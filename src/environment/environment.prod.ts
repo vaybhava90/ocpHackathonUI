20d7cd0392c4696b6fd7a0bafcf5674019cb1716
vaybhava90 <163302808+vaybhava90@users.noreply.github.com>
2025-02-09 11:22:48 +0530

    Update environment.prod.ts

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://apibankingroute-vaybhava90-dev.apps.rm1.0a51.p1.openshiftapps.com/',
   tokenName :  "authToken",
-  origin:''https://apibankingroute-vaybhava90-dev.apps.rm1.0a51.p1.openshiftapps.com' 
+  origin :'https://apibankingroute-vaybhava90-dev.apps.rm1.0a51.p1.openshiftapps.com' 
 };
